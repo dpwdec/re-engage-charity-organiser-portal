19789b6628468746f24bc3f6b6b4e5b1eab03841
dpwdec <declankolakowski@gmail.com>
2020-06-23 14:49:32 +0100

    Add react and babel.

---
+++ public/javascripts/ReactApp.js
@@ -0,0 +1,11 @@
+class ReactApp extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ReactApp />, document.getElementById('root'));
