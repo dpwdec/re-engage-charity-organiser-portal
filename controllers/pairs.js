37ebc3d48fa423c8df5050b6f1107e1c68609365
Joanne Young <joanneyoung_@hotmail.com>
2020-06-23 17:02:05 +0100

    Send back pairs from the Pairing controller hardcoded

--- controllers/pairs.js
+++ controllers/pairs.js
@@ -1,6 +1,14 @@
 var PairController = { 
   Pairing: (request, response) => {
-    response.send({someData: 'hello'});
+    response.send(
+      {
+        pairs: [
+          {id: 1, driver: 'Bradley', guest: 'Doris'},
+          {id: 2, driver: 'Zeus', guest: 'Kimothey' },
+          {id: 3, driver: 'Kevin', guest: 'Perry' }
+        ]
+      }
+    );
   }
 }
 
