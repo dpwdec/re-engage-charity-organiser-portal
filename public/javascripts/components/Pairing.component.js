fc1728fec5dbf5b570f8ecc670c2ca30d9afa7e4
Joanne Young <joanneyoung_@hotmail.com>
2020-06-23 17:14:58 +0100

    Set the pairing component state based on response from back end

--- public/javascripts/components/Pairing.component.js
+++ public/javascripts/components/Pairing.component.js
@@ -1,15 +1,24 @@
 class Pairing extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      pairs: []
+    }
+  }
+
   generatePairs = () => {
     fetch(`/pairs`)
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
+      this.setState({
+        pairs: data
+      });
     })
-
   }
 
   render() {
+    console.log(this.state)
     return (
       <button id="generate-pairs" onClick={this.generatePairs}>Generate</button>
     );
