08d230f185826c18e712ac3594a972a3bc1cabbf
dpwdec <declankolakowski@gmail.com>
2020-06-24 11:04:58 +0100

    Refactor pairings controller to extract pairs array into variable.

--- controllers/pairs.js
+++ controllers/pairs.js
@@ -1,14 +1,12 @@
 var PairController = { 
   Pairing: (request, response) => {
-    response.send(
-      {
-        pairs: [
-          {id: 1, driver: 'Bradley', guest: 'Doris'},
-          {id: 2, driver: 'Zeus', guest: 'Kimothey' },
-          {id: 3, driver: 'Kevin', guest: 'Perry' }
-        ]
-      }
-    );
+    var pairings = [
+      {id: 1, driver: 'Bradley', guest: 'Doris'},
+      {id: 2, driver: 'Zeus', guest: 'Kimothey' },
+      {id: 3, driver: 'Kevin', guest: 'Perry' }
+    ]
+
+    response.send({ pairs: pairings});
   }
 }
 
