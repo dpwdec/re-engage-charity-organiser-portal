df34be53d6039b49f3477a0c88c68b82b9a0492e
tanilserbes <serbestanil@gmail.com>
2020-06-24 15:19:43 +0100

    Initialize the adminName and password which comes from broswer

--- public/javascripts/components/adminLogin.component.js
+++ public/javascripts/components/adminLogin.component.js
@@ -1,13 +1,46 @@
 class AdminLogin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      adminName: "",
+      password: "",
+    };
+  }
+
+  handleChangeName = (e) => {
+    console.log(e.target);
+    this.setState({
+      adminName: e.target.value,
+    });
+  };
+  handleChangePassword = (e) => {
+    console.log(e.target);
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
   render() {
     return (
-      <div className="login">
+      <form id="admin-login-form" className="login" onSubmit={this.login}>
         <label>AdminName:</label>
-        <input type="text" placeholder="Admin" required></input>
+        <input
+          type="text"
+          placeholder="Admin"
+          required
+          onChange={this.handleChangeName}
+          value={this.state.adminName}
+        ></input>
         <label>Password:</label>
-        <input type="password" placeholder="Password" required></input>
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          onChange={this.handleChangePassword}
+          value={this.state.password}
+        ></input>
         <input type="submit" value="login"></input>
-      </div>
+      </form>
     );
   }
 }
