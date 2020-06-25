38a6107194cef5cb120c19b41c37345d110dda96
tracht <marija.trachtenberg@gmail.com>
2020-06-25 11:05:49 +0100

    create GuestList controller

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -16,9 +16,25 @@ var HomepageController = {
       // console.log(drivers)
       response.send(drivers);
     });
-
   },
 
+  GuestList: function(request, response) {
+    let guests = [];
+
+    Member.find(function(err, result) {
+      // console.log(result)
+      result.forEach((member) => {
+
+        if(member.role === 'guest'){
+          drivers.push(member);
+        }
+      });
+      // console.log("Guests only")
+      // console.log(guests)
+      response.send(guests);
+    });
+
+  } 
 }
 
 module.exports = HomepageController;
