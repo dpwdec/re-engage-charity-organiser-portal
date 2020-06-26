bada2c2c897df702e2428e0d3c790a6e9e706dce
tracht <marija.trachtenberg@gmail.com>
2020-06-26 14:59:04 +0100

    Add DriverAvailability component to ReactApp

--- public/javascripts/ReactApp.js
+++ public/javascripts/ReactApp.js
@@ -24,6 +24,7 @@ class ReactApp extends React.Component {
           <DriverList />
           <Pairing />
           <AdminLogOut />
+          <DriverAvailability />
         </div>
       );
     }
