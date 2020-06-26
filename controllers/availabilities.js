1db0a0e00d6a73d05925ccaf5aa1743e5176c99a
tracht <marija.trachtenberg@gmail.com>
2020-06-26 14:58:36 +0100

    add pseudocode to availability controller

--- controllers/availabilities.js
+++ controllers/availabilities.js
@@ -4,6 +4,12 @@ var AvailabilitiesController = {
 
     DriverAvailability: (request, response) => {
 
+      // go into DB and retrieve drivers
+      // get availabilities 
+      // only months = current month + next 3 months
+      // create function to determine what is the current month
+      // send back data  
+
       
     }
 }
