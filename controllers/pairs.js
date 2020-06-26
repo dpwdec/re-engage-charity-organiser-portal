1e29b043bcdb249c045475d47ab2091fbfb00f98
tanilserbes <serbestanil@gmail.com>
2020-06-26 10:38:08 +0100

    Refactor distance generation to use a signle value

--- controllers/pairs.js
+++ controllers/pairs.js
@@ -1,9 +1,9 @@
-const googleMapsClient = require('@google/maps').createClient({
-  key: 'AIzaSyC9qJYJPqeVBtCCvu68wQ286oyCL8Z5PqQ',
-  Promise: Promise
+const googleMapsClient = require("@google/maps").createClient({
+  key: "AIzaSyC9qJYJPqeVBtCCvu68wQ286oyCL8Z5PqQ",
+  Promise: Promise,
 });
 
-var PairController = { 
+var PairController = {
   Pairing: (request, response) => {
     // contact to get members
     // drivers = members.find(drivers)
@@ -43,7 +43,6 @@ var PairController = {
     // });
     // //async
 
-
     // var pairings = PairController._generatePairsByDistance(members);
 
     // members = []
@@ -66,159 +65,166 @@ var PairController = {
     // })
 
     // sort drivers and guests
-    var members = [ 
-      { name: 'Doris', 
-        drivers: [ 
-          { 
-            name: 'Bradley', 
-            distance: 8000 
-          },
-          { 
-            name: 'Zeus', 
-            distance: 2000 
+    var members = [
+      {
+        name: "Doris",
+        drivers: [
+          {
+            name: "Bradley",
+            distance: 8000,
           },
           {
-            name: 'Kevin',
-            distance: 3000
+            name: "Zeus",
+            distance: 2000,
           },
           {
-            name: 'Gwen',
-            distance: 10000
-          }
-        ]
+            name: "Kevin",
+            distance: 3000,
+          },
+          {
+            name: "Gwen",
+            distance: 10000,
+          },
+        ],
       },
-      { name: 'Kimothey', 
-        drivers: [ 
-          { 
-            name: 'Bradley', 
-            distance: 6000 
-          },
-          { 
-            name: 'Zeus', 
-            distance: 3000 
+      {
+        name: "Kimothey",
+        drivers: [
+          {
+            name: "Bradley",
+            distance: 6000,
           },
           {
-            name: 'Kevin',
-            distance: 500
+            name: "Zeus",
+            distance: 3000,
           },
           {
-            name: 'Gwen',
-            distance: 5000
-          }
-        ]
+            name: "Kevin",
+            distance: 500,
+          },
+          {
+            name: "Gwen",
+            distance: 5000,
+          },
+        ],
       },
-      { name: 'Perry', 
-        drivers: [ 
-          { 
-            name: 'Bradley', 
-            distance: 300 
-          },
-          { 
-            name: 'Zeus', 
-            distance: 10000 
+      {
+        name: "Perry",
+        drivers: [
+          {
+            name: "Bradley",
+            distance: 300,
           },
           {
-            name: 'Kevin',
-            distance: 600
+            name: "Zeus",
+            distance: 10000,
           },
           {
-            name: 'Gwen',
-            distance: 100
-          }
-        ]
+            name: "Kevin",
+            distance: 600,
+          },
+          {
+            name: "Gwen",
+            distance: 100,
+          },
+        ],
       },
-      { name: 'Petunia', 
-        drivers: [ 
-          { 
-            name: 'Bradley', 
-            distance: 300 
-          },
-          { 
-            name: 'Zeus', 
-            distance: 400 
+      {
+        name: "Petunia",
+        drivers: [
+          {
+            name: "Bradley",
+            distance: 300,
           },
           {
-            name: 'Kevin',
-            distance: 6000
+            name: "Zeus",
+            distance: 400,
           },
           {
-            name: 'Gwen',
-            distance: 5000
-          }
-        ]
-      }
-    ]
-    
+            name: "Kevin",
+            distance: 6000,
+          },
+          {
+            name: "Gwen",
+            distance: 5000,
+          },
+        ],
+      },
+    ];
+
     // randomly pair drivers and guests
     var pairings = PairController._generatePairsByDistance(members);
 
-    response.send({ pairs: pairings});
+    response.send({ pairs: pairings });
   },
   Map: (request, response) => {
-    response.render('map')
+    response.render("map");
   },
   Route: (request, response) => {
-    googleMapsClient.directions({origin: 'SW129PH', destination: 'SE153XX', mode: 'driving'})
-    .asPromise()
-    .then((result) => {
-      console.log(result);
-      response.send(result)
-    }); 
-  }
-}
+    googleMapsClient
+      .directions({
+        origin: "SW129PH",
+        destination: "SE153XX",
+        mode: "driving",
+      })
+      .asPromise()
+      .then((result) => {
+        console.log(result);
+        response.send(result);
+      });
+  },
+};
 
 // UTILITY METHODS
 
 PairController._generatePairsByDistance = (pairDistances) => {
-
-  // compare all distances 'find shorted global pair distance' 
-  // assign pair to output 
-  // eliminate that driver from all other guests 
+  // compare all distances 'find shorted global pair distance'
+  // assign pair to output
+  // eliminate that driver from all other guests
   // repeat until one pair left
-  // return output 
+  // return output
 
+  // while loop, until guests array < 1
 
-  // while loop, until guests array < 1 
+  var pairing = [];
 
-  var pairing = []
-
-  while(pairDistances.length > 1) {
-    var shortestPair = {id: 0, distance: 100000000}
-    var guestIndex; 
+  while (pairDistances.length > 0) {
+    var shortestPair = { id: 0, distance: 100000000 };
+    var guestIndex;
     var driverIndex;
 
     pairDistances.forEach((guest, i) => {
-    
       guest.drivers.forEach((driver, j) => {
         if (driver.distance < shortestPair.distance) {
-          shortestPair = { id: pairing.length + 1, 
-            driver: driver.name, 
+          shortestPair = {
+            id: pairing.length + 1,
+            driver: driver.name,
             guest: guest.name,
-            distance: driver.distance 
-          }
+            distance: driver.distance,
+          };
           guestIndex = i;
           driverIndex = j;
         }
       });
-    })
-    pairing.push(shortestPair)
-    pairDistances.splice(guestIndex, 1)
+    });
+    pairing.push(shortestPair);
+    pairDistances.splice(guestIndex, 1);
 
     pairDistances.forEach((guest) => {
-      guest.drivers.splice(driverIndex, 1)
-    })
+      guest.drivers.splice(driverIndex, 1);
+    });
   }
 
-  var lastPair = {
-    id: pairing.length + 1,
-    driver: pairDistances[0].drivers[0].name,
-    guest: pairDistances[0].name,
-    distance: pairDistances[0].drivers[0].distance
-  }
-  
-  pairing.push(lastPair)
-  console.log(pairing)
-  return pairing
+  // var lastPair = {
+  //   id: pairing.length + 1,
+  //   driver: pairDistances[0].drivers[0].name,
+  //   guest: pairDistances[0].name,
+  //   distance: pairDistances[0].drivers[0].distance,
+  // };
+
+  // pairing.push(lastPair);
+  console.log(pairing);
+  return pairing;
 };
 
 module.exports = PairController;
