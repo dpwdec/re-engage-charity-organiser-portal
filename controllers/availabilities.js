364ae44bf80e4fad61fbdf28a34a8e56a2f5e02f
tracht <marija.trachtenberg@gmail.com>
2020-06-27 20:00:57 +0100

    continue to create DriverAvailability controller method

--- controllers/availabilities.js
+++ controllers/availabilities.js
@@ -5,18 +5,32 @@ var AvailabilitiesController = {
 
     DriverAvailability: (request, response) => {
 
-      var current_month = getArrayOfCurrentAndNextThreeMonths()[0];
-      var month2 = getArrayOfCurrentAndNextThreeMonths()[1]; 
-      var month3 = getArrayOfCurrentAndNextThreeMonths()[2];
-      var month4 = getArrayOfCurrentAndNextThreeMonths()[3];
-
+      // Step 1: get current + next 3 months to be used in query
+      var current_month = getArrayOfCurrentAndNextThreeMonths()[0]; // i.e. returns June 2020
+      var month2 = getArrayOfCurrentAndNextThreeMonths()[1]; // i.e. returns July 2020
+      var month3 = getArrayOfCurrentAndNextThreeMonths()[2]; // i.e. returns Aug 2020
+      var month4 = getArrayOfCurrentAndNextThreeMonths()[3]; // i.e. returns Sept 2020
+      
+      // Step 2: Store results of Mongoose query 
+      // drivers = [ {name: "Ralph", "Jul 2020": true, "Aug 2020": false} ]
       var drivers = [];
-      
-      // Mongoose query to return 
 
-      
+      // Step 3: Mongoose query
+      Member.
+        find({role: 'driver'}).
+        select({ availability: current_month, month2, month3, month4 }).
+        exec((err, result) => {
+          response.send({ name: result.name, availabilities: result.availabilites })
+        );
     }
 }
 
 module.exports = AvailabilitiesController;
 
+
+Person.
+  find({occupation: /host/}).
+  limit(10).
+  sort({ occupation: -1 }).
+  select({ name: 1, occupation: 1 }).
+  exec(callback);
