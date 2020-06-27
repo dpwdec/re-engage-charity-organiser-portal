58be02c31d2be4c6c3a79825d97899b95ce7936d
dpwdec <declankolakowski@gmail.com>
2020-06-27 10:07:14 +0100

    Remove duplicated code from PairController.

--- controllers/pairs.js
+++ controllers/pairs.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 const Member = require("../models/member");
+const ShortestDistancePairs = require('./pairs/shortestDistancePairs');
 
 const googleMapsClient = require("@google/maps").createClient({
   key: "AIzaSyC9qJYJPqeVBtCCvu68wQ286oyCL8Z5PqQ",
@@ -31,7 +32,7 @@ var PairController = {
         });
 
         await Promise.all(allPromises); // waits for all API calls to finish
-        var pairings = PairController._generatePairsByDistance(members);
+        var pairings = ShortestDistancePairs.generate(members);
 
         response.send({ pairs: pairings });
       });
@@ -69,52 +70,6 @@ makeGoogleApiRequestForDistance = (member, guest, driver) => {
         resolve(result);
       });
   });
-};
-
-// UTILITY METHODS
-
-PairController._generatePairsByDistance = (pairDistances) => {
-  // compare all distances 'find shorted global pair distance'
-  // assign pair to output
-  // eliminate that driver from all other guests
-  // repeat until one pair left
-  // return output
-
-  // while loop, until guests array < 1
-
-  var pairing = [];
-
-  while (pairDistances.length > 0) {
-    var shortestPair = { id: 0, distance: 100000000 };
-    var guestIndex;
-    var driverName;
-
-   pairDistances.forEach((guest, i) => {
-      guest.drivers.forEach((driver, j) => {
-        if (driver.distance < shortestPair.distance) {
-          shortestPair = {
-            id: pairing.length + 1,
-            driver: driver.name,
-            guest: guest.name,
-            distance: driver.distance,
-          };
-          guestIndex = i;
-          driverName = driver.name;
-        }
-      });
-    });
-    pairing.push(shortestPair);
-    pairDistances.splice(guestIndex, 1);
-
-    pairDistances.forEach((guest) => {
-      guest.drivers.forEach((driver, index) => {
-        if(driver.name === driverName) {
-          guest.drivers.splice(index, 1);
-        }
-      });
-    });
-  }
-  return pairing;
 };
 
 module.exports = PairController;
