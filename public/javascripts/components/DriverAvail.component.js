cc01e3fa5829413ca823270b997211077c650336
tracht <marija.trachtenberg@gmail.com>
2020-06-28 11:18:14 +0100

    Updates table in DriverAvail component

--- public/javascripts/components/DriverAvail.component.js
+++ public/javascripts/components/DriverAvail.component.js
@@ -20,20 +20,27 @@ class DriverAvail extends React.Component {
 
   render() {
 
-    let data = this.state.drivers;
-
     return(
       <div>
         <h2> Driver Availabilities </h2>
-        <table className="table">
-          {data.map((driver) => {
-              <Driver 
-              key={driver.id}
-              driver={driver.name}
-              />
-            })
-          }
+        <table className="driver-avail">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th> <th>Month 1</th> <th>Month 2</th> <th>Month 3</th> <th>Month 4</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.drivers.map((driver) => (
+              < Driver name={driver.name} 
+                month1={driver.month1} month2={driver.month2}
+                month3={driver.month3} month4={driver.month4} 
+                id={driver._id} key={driver._id} />
+              ))
+            }
+          </tbody>
         </table>
+
       </div>
     );
 
