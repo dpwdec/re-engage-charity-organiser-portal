0ef46b381a1bf83af64ed23ee93b88fc3a7df4ae
tracht <marija.trachtenberg@gmail.com>
2020-06-28 11:21:39 +0100

    Updates table headers

--- public/javascripts/components/DriverAvail.component.js
+++ public/javascripts/components/DriverAvail.component.js
@@ -3,6 +3,7 @@ class DriverAvail extends React.Component {
   constructor() {
     super();
     this.state = {
+      months: [],
       drivers: [],
     };
   };
@@ -27,15 +28,22 @@ class DriverAvail extends React.Component {
           <thead>
             <tr>
               <th></th>
-              <th>Name</th> <th>Month 1</th> <th>Month 2</th> <th>Month 3</th> <th>Month 4</th>
+              <th>Name</th> 
+              <th>{this.state.months[0]}</th>
+              <th>{this.state.months[1]}</th>
+              <th>{this.state.months[2]}</th>
+              <th>{this.state.months[3]}</th>
             </tr>
           </thead>
           <tbody>
             {this.state.drivers.map((driver) => (
-              < Driver name={driver.name} 
-                month1={driver.month1} month2={driver.month2}
-                month3={driver.month3} month4={driver.month4} 
-                id={driver._id} key={driver._id} />
+              <Driver name={driver.name} 
+                month1={driver.month1} 
+                month2={driver.month2}
+                month3={driver.month3} 
+                month4={driver.month4} 
+                id={driver._id} 
+                key={driver._id} />
               ))
             }
           </tbody>
