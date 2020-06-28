96c2f58509c1c78a4f86f368e8494cb0d69a1f66
dpwdec <declankolakowski@gmail.com>
2020-06-28 13:02:25 +0100

    Genetic pairs can dispaly a single pairs.

--- controllers/pairs/geneticPairs.js
+++ controllers/pairs/geneticPairs.js
@@ -1,3 +1,12 @@
 var GeneticPairs = {};
 
+GeneticPairs.generate = (members) => {
+  return [{
+    id: 1,
+    guest: members[0].name,
+    driver: members[0].drivers[0].name,
+    distance: members[0].drivers[0].distance,
+  }]
+}
 
+module.exports = GeneticPairs;
--- spec/geneticPairs.spec.js
+++ spec/geneticPairs.spec.js
@@ -1,3 +1,23 @@
+var GeneticPairs = require('../controllers/pairs/geneticPairs');
+
 describe('Genetic Pairs', () => {
-  
+  describe('Generate', () => {
+    it('can generate a single pair', () => {
+      var data = [ 
+        { name: 'Doris', 
+          drivers: [ { 
+            name: 'Bradley', 
+            distance: 8000 
+            } 
+          ]
+        }
+      ]
+
+      var output = [
+        { id: 1, driver: 'Bradley', guest: 'Doris', distance: 8000 }
+      ]
+
+      expect(GeneticPairs.generate(data)).toEqual(output);
+    })
+  })
 });
