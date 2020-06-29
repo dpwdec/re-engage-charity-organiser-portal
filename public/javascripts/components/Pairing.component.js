580307e479b6f32b352894f39be980af74f0fe3f
tanilserbes <serbestanil@gmail.com>
2020-06-29 09:22:13 +0100

    Take pairs as a prop and send to map component

--- public/javascripts/components/Pairing.component.js
+++ public/javascripts/components/Pairing.component.js
@@ -1,38 +1,44 @@
 class Pairing extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      pairs: []
-    }
+      pairs: [],
+    };
   }
 
   generatePairs = () => {
     fetch(`/pairs`)
-    .then((response) => { 
-      return response.json() 
-    }).then((data) => {
-      this.setState({
-        pairs: data.pairs
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          pairs: data.pairs,
+        });
       });
-    })
-  }
+  };
 
   render() {
     return (
       <div>
-        <button id="generate-pairs" onClick={this.generatePairs}>Generate</button>
+        <button id="generate-pairs" onClick={this.generatePairs}>
+          Generate
+        </button>
         <div className="table">
           <table>
-            { this.state.pairs.map((pair)=> (
-                <PairItem driver={pair.driver} guest={pair.guest} distance={pair.distance} id={pair.id} key={pair.id} />
-              ))
-            }
+            {this.state.pairs.map((pair) => (
+              <PairItem
+                driver={pair.driver}
+                guest={pair.guest}
+                distance={pair.distance}
+                id={pair.id}
+                key={pair.id}
+              />
+            ))}
           </table>
+          <PairingMaps pairs={this.state.pairs} />
         </div>
       </div>
-      
-
     );
   }
 }
