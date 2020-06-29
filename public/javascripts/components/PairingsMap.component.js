6b56283ff612698b608af5023fc6cfff19fa350b
tanilserbes <serbestanil@gmail.com>
2020-06-29 09:23:32 +0100

    Initialize map and show on the browser

--- public/javascripts/components/PairingsMap.component.js
+++ public/javascripts/components/PairingsMap.component.js
@@ -1,1 +1,7 @@
-function PairingsMap(props) {}
+function PairingsMap(props) {
+  //initializing map
+  var map = new google.maps.Map(document.getElementById("map-canvas"), {
+    zoom: 12,
+    center: { lat: 51.497309, lng: -0.147165 },
+  });
+}
