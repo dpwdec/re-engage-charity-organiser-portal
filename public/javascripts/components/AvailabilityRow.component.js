90d972abfea164d8dfec230dfcf1ad42921589ef
dpwdec <declankolakowski@gmail.com>
2020-06-29 15:16:36 +0100

    Refactor DriverAvailability display.

--- public/javascripts/components/AvailabilityRow.component.js
+++ public/javascripts/components/AvailabilityRow.component.js
@@ -2,7 +2,9 @@ class AvailabilityRow extends React.Component {
   constructor() {
     super();
     this.state ={
-      iHaveMounted: false
+      months:[
+        "","","",""
+      ]
     };
   }
 
@@ -11,73 +13,26 @@ class AvailabilityRow extends React.Component {
       this.props.driver.availability = {};
     }
 
-    if(this.props.driver.availability[this.props.months[0]] != undefined) {
-      this.setState({
-        month0: this.props.driver.availability[this.props.months[0]].toString()
-      });
-    } else {
-      this.setState({
-        month0: 'undecided'
-      });
-    }
+    this.props.months.forEach((month, index) => {
+      var monthsArray = this.state.months;
 
-    if(this.props.driver.availability[this.props.months[1]] != undefined) {
-      this.setState({
-        month1: this.props.driver.availability[this.props.months[1]].toString()
-      });
-    } else {
-      this.setState({
-        month1: 'undecided'
-      });
-    }
+      monthsArray[index] = this.props.driver.availability[month] != undefined ? this.props.driver.availability[month].toString() : 'undecided'
 
-    if(this.props.driver.availability[this.props.months[2]] != undefined) {
       this.setState({
-        month2: this.props.driver.availability[this.props.months[2]].toString()
-      });
-    } else {
-      this.setState({
-        month2: 'undecided'
-      });
-    }
-
-    if(this.props.driver.availability[this.props.months[3]] != undefined) {
-      this.setState({
-        month3: this.props.driver.availability[this.props.months[3]].toString()
-      });
-    } else {
-      this.setState({
-        month3: 'undecided'
-      });
-    }
-
-    this.setState({
-      iHaveMounted: true
+        months: monthsArray
+      })
     });
   }
 
   render() {
-
-    if(this.state.iHaveMounted) {
-      console.log(this.state.month0);
-      console.log(this.props);
-    }
     return(
       <tr>
-        {this.state.iHaveMounted ? <td className="driverName">{this.props.driver.name}</td> : <td></td>}
-        {this.state.iHaveMounted ? <td className="month0">{this.state.month0}</td> : <td></td>}
-        {this.state.iHaveMounted ? <td className="month1">{this.state.month1}</td> : <td></td>}
-        {this.state.iHaveMounted ? <td className="month2">{this.state.month2}</td> : <td></td>}
-        {this.state.iHaveMounted ? <td className="month3">{this.state.month3}</td> : <td></td>}
+        <td className="driverName">{this.props.driver.name}</td>
+        <td className="month0">{this.state.months[0]}</td>
+        <td className="month1">{this.state.months[1]}</td>
+        <td className="month2">{this.state.months[2]}</td>
+        <td className="month3">{this.state.months[3]}</td>
       </tr>
-    // <tr>
-    //   <td>{props._id}</td>
-    //   <td className="driverName">{this.props.driver.name}</td>
-    //   <td className="month0">{this.state.month0}</td>
-    //   <td className="month0">NA</td>
-    //   <td className="month0">NA</td>
-    //   <td className="month0">NA</td>
-    // </tr>
     )
   }
 }
--- public/javascripts/components/DriverAvailability.component.js
+++ public/javascripts/components/DriverAvailability.component.js
@@ -24,35 +24,23 @@ class DriverAvailability extends React.Component {
   render() {
 
 
-
-    // if month is undefined, return 'unconfirmed' 
-    // else return value -> t/f
-
-
     return (
       <div>
         <h2> Driver Availabilities </h2>
         <table className="driver-avail">
           <thead>
             <tr>
-              <th>Name</th> 
-              <th>{this.state.months[0]}</th>
-              <th>{this.state.months[1]}</th>
-              <th>{this.state.months[2]}</th>
-              <th>{this.state.months[3]}</th>
+              <th>Name</th>
+              {this.state.months.map((month) => (
+                <th>{month}</th>
+              ))
+              }
             </tr>
           </thead>
           <tbody>
 
             {this.state.drivers.map((driver) => (
               <AvailabilityRow driver={driver} months={this.state.months}/>
-              // <tr>
-              //   <td> {driver.name} </td>
-              //   { driver.availability ? <td> {driver.availability[this.state.months[0]]} </td> : <td> unconfirmed </td> }
-              //   { driver.availability ? <td> {driver.availability[this.state.months[1]]} </td> : <td> unconfirmed </td> }
-              //   { driver.availability ? <td> {driver.availability[this.state.months[2]]} </td> : <td> unconfirmed </td> }
-              //   { driver.availability ? <td> {driver.availability[this.state.months[3]]} </td> : <td> unconfirmed </td> }
-              // </tr>
               ))
             }
           </tbody>
