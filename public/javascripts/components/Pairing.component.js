50458bb2f0f054c323a7969e273ec56067accb51
tanilserbes <serbestanil@gmail.com>
2020-06-30 11:30:45 +0100

    Add months dropdown menu

--- public/javascripts/components/Pairing.component.js
+++ public/javascripts/components/Pairing.component.js
@@ -25,6 +25,20 @@ class Pairing extends React.Component {
         <button id="generate-pairs" onClick={this.generatePairs}>
           Generate
         </button>
+        <select id="month">
+          <option value="1">January</option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+          <option value="4">April</option>
+          <option value="5">May</option>
+          <option value="6">June</option>
+          <option value="7">July</option>
+          <option value="8">August</option>
+          <option value="9">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
         <div className="table">
           <table>
             <thead></thead>
