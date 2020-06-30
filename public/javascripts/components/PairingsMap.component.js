35102b6c2888748a7a60b7809dd54f63a92bd8af
tanilserbes <serbestanil@gmail.com>
2020-06-30 23:52:57 +0100

    Add new marker objects

--- public/javascripts/components/PairingsMap.component.js
+++ public/javascripts/components/PairingsMap.component.js
@@ -1,56 +1,69 @@
 function PairingsMap(props) {
-  //initializing map
+  /* For more information please visit: https://developers.google.com/maps/documentation
+  Here some resource helped me to make api works;
+  DirectionsServices: https://developers.google.com/maps/documentation/javascript/directions
+  Drawing Marker: https://developers.google.com/maps/documentation/javascript/examples/icon-complex
+  Code samples: https://developers.google.com/maps/documentation/javascript/examples
+  */
   var map = new google.maps.Map(document.getElementById("map-canvas"), {
     zoom: 12,
     center: { lat: 51.497309, lng: -0.147165 },
   });
+
   var icons = {
     start: new google.maps.MarkerImage(
-      // URL
-      // (width,height)
-      new google.maps.Size(44, 32),
-      // The origin point (x,y)
+      "https://img.icons8.com/fluent/48/000000/marker-storm.png",
+      new google.maps.Size(20, 32),
       new google.maps.Point(0, 0),
-      // The anchor point (x,y)
-      new google.maps.Point(22, 32)
+      new google.maps.Point(0, 32)
     ),
+
     end: new google.maps.MarkerImage(
-      // URL
-      // (width,height)
-      new google.maps.Size(44, 32),
-      // The origin point (x,y)
+      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+      new google.maps.Size(20, 32),
       new google.maps.Point(0, 0),
-      // The anchor point (x,y)
-      new google.maps.Point(22, 32)
+      new google.maps.Point(0, 32)
     ),
   };
-  var makeMarker = (position, icon, title) => {
+
+  var startMarker = (position, icon, title) => {
     new google.maps.Marker({
-      position: position,
       map: map,
+      position: position,
+      icon: icon,
+      label: title,
+    });
+  };
+  var endMarker = (position, icon, title) => {
+    new google.maps.Marker({
+      map: map,
+      position: position,
       icon: icon,
       label: title,
     });
   };
   props.pairs.forEach((pair) => {
+    console.log(pair.route.routes[0].legs[0]);
     var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer();
+    var directionsRenderer = new google.maps.DirectionsRenderer({
+      suppressMarkers: true,
+    });
     directionsRenderer.setMap(map);
 
     var request = {
-      origin: { query: pair.route.routes[0].legs[0].start_address },
-      destination: { query: pair.route.routes[0].legs[0].end_address },
+      origin: pair.route.routes[0].legs[0].start_location,
+      destination: pair.route.routes[0].legs[0].end_location,
       travelMode: "DRIVING",
     };
     directionsService.route(request, function (response, status) {
       if (status === "OK") {
         directionsRenderer.setDirections(response);
-        makeMarker(
+        startMarker(
           pair.route.routes[0].legs[0].start_location,
           icons.start,
           pair.driver
         );
-        makeMarker(
+        endMarker(
           pair.route.routes[0].legs[0].end_location,
           icons.end,
           pair.guest
