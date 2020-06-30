b74b45b0e4dda56d58721fe90b1ba58df570aa8e
tracht <marija.trachtenberg@gmail.com>
2020-06-30 16:46:24 +0100

    Correctly update years when generating current and next 3 months

--- controllers/helperFunctions.js
+++ controllers/helperFunctions.js
@@ -7,12 +7,11 @@ helperFunction.getArrayOfCurrentAndNextThreeMonths = () => {
   
   //helpers
   var today = new Date();
-  var yyyy = today.getFullYear();  // returns year as number, i.e. 2020
+  var yyyy0 = today.getFullYear();  // returns year as number, i.e. 2020
   // returns current month as a number
   var mm = Number(String(today.getMonth() + 1).padStart(2, '0'));
 
-  var monthsHash = { 1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 
-  7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" }
+  var monthsHash = { 1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec" }
 
   // returns months as a string, i.e "Jan"
   var mm0 = monthsHash[Number(String(today.getMonth() + 1).padStart(2, '0'))]; 
@@ -20,8 +19,25 @@ helperFunction.getArrayOfCurrentAndNextThreeMonths = () => {
   var mm2 = monthsHash[mm + 2];
   var mm3 = monthsHash[mm + 3];
 
+  var yyyy1 = yyyy0; 
+  var yyyy2 = yyyy0;
+  var yyyy3 = yyyy0;
+
+  // updating years 
+  if (mm0 === 'Oct') { yyyy3 = (yyyy0 + 1) } 
+  if (mm0 === 'Nov') {
+    yyyy2 = (yyyy0 + 1)
+    yyyy3 = (yyyy0 + 1)
+  } 
+  if (mm0 === 'Dec') {
+    yyyy1 = (yyyy0 + 1)
+    yyyy2 = (yyyy0 + 1)
+    yyyy3 = (yyyy0 + 1)
+  } 
+
+
   // returns an array of 4 consecutive months as strings, i.e. ["Jan 2020", "Feb 2020", "Mar 2020", "Apr 2020"]
-  var result = [`${mm0} ${yyyy}`, `${mm1} ${yyyy}`, `${mm2} ${yyyy}`, `${mm3} ${yyyy}`];
+  var result = [`${mm0} ${yyyy0}`, `${mm1} ${yyyy1}`, `${mm2} ${yyyy2}`, `${mm3} ${yyyy3}`];
 
   return result;
 };
@@ -33,7 +49,6 @@ helperFunction.getCurrentYearAsNumber = () => {
   console.log(yyyy)
   return yyyy;
 }
-
 
 helperFunction.getCurrentMonthAsNumber = () => {
   var today = new Date();
