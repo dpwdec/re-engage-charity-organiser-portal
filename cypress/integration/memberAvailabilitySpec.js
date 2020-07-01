14c021da90ad84c0bd549fc06e88ab50c5ba19bf
tanilserbes <serbestanil@gmail.com>
2020-07-01 10:12:15 +0100

    Update

--- cypress/integration/memberAvailabilitySpec.js
+++ cypress/integration/memberAvailabilitySpec.js
@@ -1,12 +1,24 @@
 describe("DriverAvailability", () => {
   beforeEach(function () {
-    cy.task('dropAdmins');
-    cy.task('addAdmin', { adminName: "admin", password: "1234" });
+    cy.task("dropAdmins");
+    cy.task("addAdmin", { adminName: "admin", password: "1234" });
 
-    cy.task('dropMembers');
-    cy.task('addMember', {name: 'Cat', role: 'driver', availability: { "Jun 2020": true, "Jul 2020": false, "Aug 2020": false, "Sep 2020": true }})
-    cy.task('addMember', {name: 'Merija', role: 'guest', availability: { "Jul 2020": true, "Aug 2020": true, "Sep 2020": true }})
-
+    cy.task("dropMembers");
+    cy.task("addMember", {
+      name: "Cat",
+      role: "driver",
+      availability: {
+        "Jun 2020": false,
+        "Jul 2020": false,
+        "Aug 2020": false,
+        "Sep 2020": true,
+      },
+    });
+    cy.task("addMember", {
+      name: "Merija",
+      role: "guest",
+      availability: { "Jul 2020": true, "Aug 2020": true, "Sep 2020": true },
+    });
 
     cy.visit("admin/login");
     cy.get("#admin").type("admin");
@@ -17,19 +29,23 @@ describe("DriverAvailability", () => {
   it("displays all drivers availability", () => {
     // Simplest test created to check the controller logic
     cy.visit("/");
-    cy.contains('Cat').parents('tr').within(() => {
-      cy.get('td').eq(1).contains('true');
-      cy.get('td').eq(2).contains('false');
-      cy.get('td').eq(3).contains('false');
-      cy.get('td').eq(4).contains('true');
-    })
+    cy.contains("Cat")
+      .parents("tr")
+      .within(() => {
+        cy.get("td").eq(1).contains("false");
+        cy.get("td").eq(2).contains("false");
+        cy.get("td").eq(3).contains("false");
+        cy.get("td").eq(4).contains("true");
+      });
 
-    cy.contains('Merija').parents('tr').within(() => {
-      cy.get('td').eq(1).contains('undecided');
-      cy.get('td').eq(2).contains('true');
-      cy.get('td').eq(3).contains('true');
-      cy.get('td').eq(4).contains('true');
-    })
+    cy.contains("Merija")
+      .parents("tr")
+      .within(() => {
+        cy.get("td").eq(1).contains("undecided");
+        cy.get("td").eq(2).contains("true");
+        cy.get("td").eq(3).contains("true");
+        cy.get("td").eq(4).contains("true");
+      });
 
     cy.get("#logout").click();
   });
