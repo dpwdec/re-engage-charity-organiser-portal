c15c2fc201804b8e47f06224263c4e52da1f833f
tanilserbes <serbestanil@gmail.com>
2020-07-01 10:16:23 +0100

    Update

--- cypress/integration/memberAvailabilitySpec.js
+++ cypress/integration/memberAvailabilitySpec.js
@@ -1,16 +1,19 @@
 describe("DriverAvailability", () => {
   beforeEach(function () {
-<<<<<<< HEAD
     cy.task("dropAdmins");
     cy.task("addAdmin", { adminName: "admin", password: "1234" });
-=======
-    cy.task('dropAdmins');
-    cy.task('addAdmin', { adminName: "admin", password: "1234" });
 
-    cy.task('dropMembers');
-    cy.task('addMember', {name: 'Cat', role: 'driver', availability: { "Jul 2020": false, "Aug 2020": false, "Sep 2020": true }})
-    cy.task('addMember', {name: 'Merija', role: 'guest', availability: { "Jul 2020": true, "Aug 2020": true, "Sep 2020": true }})
->>>>>>> d20ba357e074e90ac84c16810f069cb4ece05e4e
+    cy.task("dropMembers");
+    cy.task("addMember", {
+      name: "Cat",
+      role: "driver",
+      availability: { "Jul 2020": false, "Aug 2020": false, "Sep 2020": true },
+    });
+    cy.task("addMember", {
+      name: "Merija",
+      role: "guest",
+      availability: { "Jul 2020": true, "Aug 2020": true, "Sep 2020": true },
+    });
 
     cy.task("dropMembers");
     cy.task("addMember", {
@@ -38,39 +41,23 @@ describe("DriverAvailability", () => {
   it("displays all drivers availability", () => {
     // Simplest test created to check the controller logic
     cy.visit("/");
-<<<<<<< HEAD
     cy.contains("Cat")
       .parents("tr")
       .within(() => {
-        cy.get("td").eq(1).contains("false");
-        cy.get("td").eq(2).contains("false");
-        cy.get("td").eq(3).contains("false");
-        cy.get("td").eq(4).contains("true");
+        cy.get("td").eq(1).contains("False");
+        cy.get("td").eq(2).contains("False");
+        cy.get("td").eq(3).contains("True");
+        cy.get("td").eq(4).contains("TBD");
       });
 
     cy.contains("Merija")
       .parents("tr")
       .within(() => {
-        cy.get("td").eq(1).contains("undecided");
-        cy.get("td").eq(2).contains("true");
-        cy.get("td").eq(3).contains("true");
-        cy.get("td").eq(4).contains("true");
+        cy.get("td").eq(1).contains("True");
+        cy.get("td").eq(2).contains("True");
+        cy.get("td").eq(3).contains("True");
+        cy.get("td").eq(4).contains("TBD");
       });
-=======
-    cy.contains('Cat').parents('tr').within(() => {
-      cy.get('td').eq(1).contains('False');
-      cy.get('td').eq(2).contains('False');
-      cy.get('td').eq(3).contains('True');
-      cy.get('td').eq(4).contains('TBD');
-    })
-
-    cy.contains('Merija').parents('tr').within(() => {
-      cy.get('td').eq(1).contains('True');
-      cy.get('td').eq(2).contains('True');
-      cy.get('td').eq(3).contains('True');
-      cy.get('td').eq(4).contains('TBD');
-    })
->>>>>>> d20ba357e074e90ac84c16810f069cb4ece05e4e
 
     cy.get("#logout").click();
   });
