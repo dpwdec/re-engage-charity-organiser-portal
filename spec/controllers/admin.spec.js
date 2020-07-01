29c4f1850cc93ac6bc03eecf34a7391617c49a09
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-01 15:24:37 +0100

    Start test for successful login

--- spec/controllers/admin.spec.js
+++ spec/controllers/admin.spec.js
@@ -12,9 +12,7 @@ describe('Admin Controller', () => {
         }
       }
 
-      var res = {
-        send: jest.fn()
-      }
+      var res = { send: jest.fn() }
       var req = { body: { adminName: 'mock', password: 'mock'} };
 
       controller = AdminController.Login(mockAdminModel);
@@ -24,6 +22,25 @@ describe('Admin Controller', () => {
         success: false,
         message: "Datebase error",
       });
+    });
+  });
+
+  it('logs in successfully', () => {
+    var mockAdminModel = {
+      findOne: (query, callback) => {
+        var err = false;
+        var result = { adminName: 'admin', password: '1234' }
+        callback(true, result);
+      }
+    }
+    var res = { send: jest.fn() }
+    var req = { body: { adminName: 'admin', password: '1234'} };
+    controller = AdminController.Login(mockAdminModel);
+    controller(req, res);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: "Valid log in",
+      admin: { adminName: 'admin', password: '1234' }
     });
   });
 });
