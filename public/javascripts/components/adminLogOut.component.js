c03184cfb1d1893cdf12befda9b48163b63e40da
tracht <marija.trachtenberg@gmail.com>
2020-07-28 12:46:03 +0200

    Removes commented out code.

--- public/javascripts/components/adminLogOut.component.js
+++ public/javascripts/components/adminLogOut.component.js
@@ -12,19 +12,9 @@ class AdminLogOut extends React.Component {
   render() {
     return (
       <div id="logout-button">
-        {/* <form id="logout" className="logout" onSubmit={this.logout}>
-          <input
-            id="logout"
-            className="button login100-form-btn-small" 
-            type="submit"
-            value="log out"
-          ></input>
-        </form> */}
-        {/* <div className="container-login100-form-btn"> */}
           <button id="logout" className="login100-form-btn-small" onClick={this.logout}>
             logout
           </button>
-        {/* </div> */}
       </div>
       
     );
