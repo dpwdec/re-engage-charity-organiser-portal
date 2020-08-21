bd2d9d3cdf210ca65fbbd9f41c8e13f45af36dfc
Dec Kolakowski <declankolakowski@gmail.com>
2020-08-21 21:20:30 +0100

    update tab sizes

--- spec/controllers/availability.spec.js
+++ spec/controllers/availability.spec.js
@@ -1,28 +1,28 @@
 const AvailabilityController = require('../../controllers/availability');
 
 describe("Availability Controller", () => {
-    describe("Availability", () => {
-        it("executes a response", () => {
-            /*
-            // Previous implementation:
-            const Member = {
-                find: () => { 
-                    return { lean: () => { return { exec: (callback) => callback(false, {}) } } }
-                }
-            */
-            const Member = { find: jest.fn() };
-            Member.find.mockReturnValue({ lean: jest.fn() });
-            Member.find().lean.mockReturnValue({ exec: (callback) => callback(false, {})});
+  describe("Availability", () => {
+    it("executes a response", () => {
+      /*
+      // Previous implementation:
+      const Member = {
+          find: () => { 
+              return { lean: () => { return { exec: (callback) => callback(false, {}) } } }
+          }
+      */
+      const Member = { find: jest.fn() };
+      Member.find.mockReturnValue({ lean: jest.fn() });
+      Member.find().lean.mockReturnValue({ exec: (callback) => callback(false, {})});
 
-            controller = AvailabilityController.Availability(Member);
+      controller = AvailabilityController.Availability(Member);
 
-            let req = { query: { role: 'driver' } }
-            let res = { send: jest.fn() };
+      let req = { query: { role: 'driver' } }
+      let res = { send: jest.fn() };
 
-            controller(req, res);
+      controller(req, res);
 
-            expect(res.send).toHaveBeenCalledWith({ members: {}, months: ["Aug 2020", "Sep 2020", "Oct 2020", "Nov 2020"] });
+      expect(res.send).toHaveBeenCalledWith({ members: {}, months: ["Aug 2020", "Sep 2020", "Oct 2020", "Nov 2020"] });
 
-        });
     });
+  });
 });
