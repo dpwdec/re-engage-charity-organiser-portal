807a55d6249d0a91b1bbdd0714dd5bb044fcaff9
Dec Kolakowski <declankolakowski@gmail.com>
2020-08-26 16:22:20 +0100

    Update tests for pairing controller with DI

--- controllers/pairs.js
+++ controllers/pairs.js
@@ -10,88 +10,86 @@ const googleMapsClient = require("@google/maps").createClient({
 });
 
 var PairController = {
-  Pairing: (Member, GoogleMapsClient) => async (request, response) => {
+  Pairing: (Member, GoogleMaps, ShortestDistancePairs, AveragePairs, PairingPopulation) => async (request, response) => {
     let month = request.query.month;
     let query = {};
     query[month] = true;
 
     let availableMembers = PairController.availableMembers(Member, request.query.month);
 
-    Member.find({ role: "guest" }, (err, guests) => {
-      var availableGuests = [];
-      guests.forEach((guest) => {
-        if (guest.availability[month] !== undefined) {
-          if (guest.availability[month] === true) {
-            availableGuests.push(guest);
-          }
-        }
-      });
-      Member.find({ role: "driver" }, async (err, drivers) => {
-        var availableDrivers = [];
-        drivers.forEach((driver) => {
-          if (driver.availability[month] !== undefined) {
-            if (driver.availability[month] === true) {
-              availableDrivers.push(driver);
-            }
-          }
-        });
+    let members = await PairController.calculatePairDistances(GoogleMaps, availableMembers);
 
-        var members = [];
-        var allPromises = [];
+    // use different pairing alogirithm depending on user input
+    var pairings;
+    if (request.query.pairingType === "shortest") {
+      pairings = ShortestDistancePairs.generate(members);
+    } else if (request.query.pairingType === "average") {
+      pairings = AveragePairs.generate(members);
+    } else {
+      pairings = PairingPopulation.generate(members);
+    }
 
-        availableGuests.forEach((guest) => {
-          var member = {
-            name: guest.name,
-            telephone: guest.telephone,
-            drivers: [],
-          };
+    response.status(200).send({ pairs: pairings });
 
-          var driverGuestPairPromises = availableDrivers.map((driver) => {
-            return makeGooglePairRouteApiRequest(member, guest, driver);
-          });
+    // Member.find({ role: "guest" }, (err, guests) => {
+    //   var availableGuests = [];
+    //   guests.forEach((guest) => {
+    //     if (guest.availability[month] !== undefined) {
+    //       if (guest.availability[month] === true) {
+    //         availableGuests.push(guest);
+    //       }
+    //     }
+    //   });
+    //   Member.find({ role: "driver" }, async (err, drivers) => {
+    //     var availableDrivers = [];
+    //     drivers.forEach((driver) => {
+    //       if (driver.availability[month] !== undefined) {
+    //         if (driver.availability[month] === true) {
+    //           availableDrivers.push(driver);
+    //         }
+    //       }
+    //     });
 
-          driverGuestPairPromises.forEach((APIpromise) => {
-            allPromises.push(APIpromise);
-          });
+    //     var members = [];
+    //     var allPromises = [];
 
-          members.push(member);
-        });
+    //     availableGuests.forEach((guest) => {
+    //       var member = {
+    //         name: guest.name,
+    //         telephone: guest.telephone,
+    //         drivers: [],
+    //       };
 
-        await Promise.all(allPromises); // waits for all API calls to finish
+    //       var driverGuestPairPromises = availableDrivers.map((driver) => {
+    //         return makeGooglePairRouteApiRequest(member, guest, driver);
+    //       });
 
-        // use different pairing alogirithm depending on user input
-        var pairings;
-        if (request.query.pairingType === "shortest") {
-          console.log("generating pairs with shortest heuristic");
-          pairings = ShortestDistancePairs.generate(members);
-        } else if (request.query.pairingType === "average") {
-          console.log("generating pairs with average heuristic");
-          pairings = AveragePairs.generate(members);
-        } else {
-          console.log("generating pairs with smart heuristic");
-          pairings = PairingPopulation.generate(members);
-        }
+    //       driverGuestPairPromises.forEach((APIpromise) => {
+    //         allPromises.push(APIpromise);
+    //       });
 
-        response.send({ pairs: pairings });
-      });
-    });
+    //       members.push(member);
+    //     });
+
+    //     await Promise.all(allPromises); // waits for all API calls to finish
+
+    //     // use different pairing alogirithm depending on user input
+    //     var pairings;
+    //     if (request.query.pairingType === "shortest") {
+    //       console.log("generating pairs with shortest heuristic");
+    //       pairings = ShortestDistancePairs.generate(members);
+    //     } else if (request.query.pairingType === "average") {
+    //       console.log("generating pairs with average heuristic");
+    //       pairings = AveragePairs.generate(members);
+    //     } else {
+    //       console.log("generating pairs with smart heuristic");
+    //       pairings = PairingPopulation.generate(members);
+    //     }
+
+    //     response.send({ pairs: pairings });
+    //   });
+    // });
   },
-  // Map: (request, response) => {
-  //   response.render("map");
-  // },
-  // Route: (request, response) => {
-  //   googleMapsClient
-  //     .directions({
-  //       origin: "SW129PH",
-  //       destination: "SE153XX",
-  //       mode: "driving",
-  //     })
-  //     .asPromise()
-  //     .then((result) => {
-  //       console.log(result);
-  //       response.send(result);
-  //     });
-  // },
 
   // (Member, String) -> { drivers: [Member], guests: [Member] }
   availableMembers: async (Member, month) => {
@@ -115,6 +113,57 @@ var PairController = {
     });
 
     return available;
+  },
+
+  // (GoogleApi, { drivers: [Member], guests: [Member] }) -> [Object]
+  calculatePairDistances: async (GoogleMaps, availableMembers) => {
+    let GoogleMapsClient = GoogleMaps.createClient({
+      key: process.env.REACT_APP_MAP_API_KEY,
+      Promise: Promise,
+    });
+
+    var members = [];
+    var allPromises = [];
+
+    availableMembers.guests.forEach((guest) => {
+      var member = {
+        name: guest.name,
+        telephone: guest.telephone,
+        drivers: [],
+      };
+
+      var driverGuestPairPromises = availableMembers.drivers.map((driver) => {
+        return PairController.mapsPairApiRequest(GoogleMapsClient, member, guest, driver);
+      });
+
+      driverGuestPairPromises.forEach((APIpromise) => {
+        allPromises.push(APIpromise);
+      });
+
+      members.push(member);
+    });
+
+    await Promise.all(allPromises); // waits for all API calls to finish
+
+    return members;
+  },
+
+  // (GoogleApiMapsClient, Object, Object, Object) -> Object
+  mapsPairApiRequest: (GoogleMapsClient, member, guest, driver) => {
+    return new Promise(function (resolve) {
+      GoogleMapsClient
+        .directions({ origin: guest.address, destination: driver.address })
+        .asPromise()
+        .then((result) => {
+          //add the driver to the guest object
+          member.drivers.push({
+            name: driver.name,
+            distance: result.json.routes[0].legs[0].distance.value,
+            route: result.json,
+          });
+          resolve(result);
+        });
+    });
   }
 };
 
@@ -137,3 +186,20 @@ makeGooglePairRouteApiRequest = (member, guest, driver) => {
 
 
 module.exports = PairController;
+
+  // Map: (request, response) => {
+  //   response.render("map");
+  // },
+  // Route: (request, response) => {
+  //   googleMapsClient
+  //     .directions({
+  //       origin: "SW129PH",
+  //       destination: "SE153XX",
+  //       mode: "driving",
+  //     })
+  //     .asPromise()
+  //     .then((result) => {
+  //       console.log(result);
+  //       response.send(result);
+  //     });
+  // },
--- spec/controllers/pairs.spec.js
+++ spec/controllers/pairs.spec.js
@@ -1,51 +1,99 @@
 jest.mock('../../models/member');
-jest.mock("@google/maps")
+jest.mock("@google/maps");
 let Member = require('../../models/member');
 let GoogleMaps = require("@google/maps");
 
-let PairController = require('../../controllers/pairs');
+jest.mock("../../controllers/pairs/shortestDistancePairs");
+jest.mock("../../controllers/pairs/averagePairs");
+jest.mock("../../controllers/pairs/pairingPopulation");
+let ShortestDistancePairs = require("../../controllers/pairs/shortestDistancePairs");
+let AveragePairs = require("../../controllers/pairs/averagePairs");
+let PairingPopulation = require("../../controllers/pairs/pairingPopulation");
 
 describe("Members Controller", () => {
-  let req, res;
+  let req, res, PairController;
 
   beforeEach(() => {
+    jest.resetModules();
+    PairController = require('../../controllers/pairs');
+
     res = { status: jest.fn() }
     res.status.mockReturnValue({ send: jest.fn() })
     req = { query: { month: "July" } };
-
-    let members = [
-      { name: "Doris", role: "guest", telephone: "0", address: "X", availability: {
-          "July 2020": true
-        }
-      },
-      { name: "Petunia", role: "guest", telephone: "0", address: "X", availability: {
-          "July 2020": false
-        }
-      },
-      { name: "Bradley", role: "driver", telephone: "0", address: "X", availability: {
-          "July 2020": false
-        }
-      },
-      { name: "Zeus", role: "driver", telephone: "0", address: "X", availability: {
-          "July 2020": true
-        }
-     },
-    ]
-    Member.find().lean.mockReturnValue({ exec: () => Promise.resolve(members) });
   });
 
-  // describe("Pairing", () => {
-  //   it("Generates a set of pairs using the shortest path algorithm", async () => {
-  //     controller = PairController.Pairing(Member, GoogleMaps);
+  describe("Pairing", () => {
+    it("Generates a set of pairs using the shortest path algorithm", async () => {
+      PairController.availableMembers = jest.fn();
+      PairController.calculatePairDistances = jest.fn();
+      PairController.calculatePairDistances.mockReturnValue(Promise.resolve({ members: "data" }));
+      ShortestDistancePairs.generate.mockReturnValue({ route: "data" });
+      controller = PairController.Pairing(Member, jest.fn(), ShortestDistancePairs, jest.fn(), jest.fn());
+      req.query.pairingType = "shortest";
 
-  //     await controller(req, res);
+      await controller(req, res);
 
-  //     expect(Member.find).toHaveBeenCalled();
-  //   });
-  // });
+      expect(PairController.availableMembers).toHaveBeenCalled();
+      expect(PairController.calculatePairDistances).toHaveBeenCalled();
+      expect(ShortestDistancePairs.generate).toHaveBeenCalledWith({ members: "data" });
+      expect(res.status().send).toHaveBeenCalledWith({ pairs: { route: "data" } });
+    });
+
+    it("Generates a set of pairs using the average path algorith", async () => { 
+      PairController.availableMembers = jest.fn();
+      PairController.calculatePairDistances = jest.fn();
+      PairController.calculatePairDistances.mockReturnValue(Promise.resolve({ members: "data" }));
+      AveragePairs.generate.mockReturnValue({ route: "data" });
+      controller = PairController.Pairing(Member, jest.fn(), jest.fn(), AveragePairs, jest.fn());
+      req.query.pairingType = "average";
+
+      await controller(req, res);
+
+      expect(PairController.availableMembers).toHaveBeenCalled();
+      expect(PairController.calculatePairDistances).toHaveBeenCalled();
+      expect(AveragePairs.generate).toHaveBeenCalledWith({ members: "data" });
+      expect(res.status().send).toHaveBeenCalledWith({ pairs: { route: "data" } });
+    });
+
+    it("Generates a set of pairs using the smart path algorithm", async () => {
+      PairController.availableMembers = jest.fn();
+      PairController.calculatePairDistances = jest.fn();
+      PairController.calculatePairDistances.mockReturnValue(Promise.resolve({ members: "data" }));
+      PairingPopulation.generate.mockReturnValue({ route: "data" });
+      controller = PairController.Pairing(Member, jest.fn(), jest.fn(), jest.fn(), PairingPopulation);
+      req.query.pairingType = "smart";
+
+      await controller(req, res);
+
+      expect(PairController.availableMembers).toHaveBeenCalled();
+      expect(PairController.calculatePairDistances).toHaveBeenCalled();
+      expect(PairingPopulation.generate).toHaveBeenCalledWith({ members: "data" });
+      expect(res.status().send).toHaveBeenCalledWith({ pairs: { route: "data" } });
+    });
+  });
 
   describe("availableMembers", () => {
     it("Returns a list of  available drivers and guests by date", async () => {
+      let members = [
+        { name: "Doris", role: "guest", telephone: "0", address: "X", availability: {
+            "July 2020": true
+          }
+        },
+        { name: "Petunia", role: "guest", telephone: "0", address: "X", availability: {
+            "July 2020": false
+          }
+        },
+        { name: "Bradley", role: "driver", telephone: "0", address: "X", availability: {
+            "July 2020": false
+          }
+        },
+        { name: "Zeus", role: "driver", telephone: "0", address: "X", availability: {
+            "July 2020": true
+          }
+       },
+      ]
+      Member.find().lean.mockReturnValue({ exec: () => Promise.resolve(members) });
+
       let availableMembers = await PairController.availableMembers(Member, "July 2020");
 
       expect(availableMembers.drivers.length).toEqual(1);
@@ -53,4 +101,105 @@ describe("Members Controller", () => {
       expect(Member.find).toHaveBeenCalled();
     });
   });
+
+  describe("calculatePairDistances", () => {
+    it("returns a list of guests with distances to drivers", async () => {
+      // Arrange
+      GoogleMaps.createClient.mockReturnValue({ directions: jest.fn() });
+      GoogleMaps.createClient().directions.mockReturnValue({ asPromise: jest.fn() })
+      GoogleMaps.createClient().directions().asPromise.mockReturnValue(
+        Promise.resolve(
+          {
+            json: {
+              routes: [
+                {
+                  legs: [
+                    {
+                      distance: {
+                        value: 20
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        )
+      );
+      let availableMembers = {
+        guests: [
+          {
+            name: "Doris",
+            telephone: "xxxxx",
+            address: "X1 XXX"
+          }
+        ],
+        drivers: [
+          {
+            name: "Bradley",
+            address: "X2 XXX"
+          },
+          {
+            name: "Zeus",
+            address: "X3 XXX"
+          },
+        ]
+      };
+
+      // Act
+      let members = await PairController.calculatePairDistances(GoogleMaps, availableMembers);
+      
+      // Assert
+      expect(members.length).toEqual(1);
+      expect(members[0].drivers.length).toEqual(2);
+      expect(members[0].drivers[1].distance).toEqual(20);
+    });
+  });
+
+  describe("mapsPairApiRequest", () => {
+    it("adds the distance between guest and driver to a member", async () => {
+      // Arrange
+      GoogleMaps.createClient.mockReturnValue({ directions: jest.fn() });
+      GoogleMaps.createClient().directions.mockReturnValue({ asPromise: jest.fn() })
+      GoogleMaps.createClient().directions().asPromise.mockReturnValue(
+        Promise.resolve(
+          {
+            json: {
+              routes: [
+                {
+                  legs: [
+                    {
+                      distance: {
+                        value: 20
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        )
+      );
+      let member = {
+        name: "Doris",
+        telephone: "x",
+        drivers: [],
+      };
+      let guest = {
+        address: "X1 XXX"
+      }
+      let driver = {
+        name: "Bradley",
+        address: "X2 XXX"
+      }
+
+      // Act
+      let result = await Promise.resolve(
+        PairController.mapsPairApiRequest(GoogleMaps.createClient(), member, guest, driver)
+      );
+
+      // Assert
+      expect(member.drivers[0].name).toEqual("Bradley");
+    });
+  })
 });
